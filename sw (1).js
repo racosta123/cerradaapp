d75114edf50d386645714996d4da266e59513422
racosta123 <acosta4770@gmail.com>
2026-04-28 01:52:44 -0700

    Add files via upload

---
+++ sw (1).js
@@ -0,0 +1,36 @@
+const CACHE = 'cerradaapp-v1';
+const ASSETS = [
+  '/cerradaapp/',
+  '/cerradaapp/index.html'
+];
+
+// Instalar y cachear
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+// Activar y limpiar caches viejos
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch: red primero, cache como respaldo
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(cache => cache.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
